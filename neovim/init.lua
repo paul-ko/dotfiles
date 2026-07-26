ea563715b37cd78bd1400bb460640e7816c6277a
Paul K <pmkoenig@gmail.com>
2026-07-25 20:28:35 -0700

    Clean up nvim cfg

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,9 +1,12 @@
--- See
--- - https://neovim.io/doc/user/lua-guide/
--- - https://github.com/jdhao/nvim-config
+--[[
+See
+  - https://neovim.io/doc/user/lua-guide/
+  - https://github.com/jdhao/nvim-config
+]]
 
 vim.filetype.add({
   extension = {
+    -- Enables syntax highlighting in .gitconfig files
     gitconfig = "gitconfig",
   },
 })
@@ -18,18 +21,23 @@ vim.opt.shiftwidth=4        -- use 4 spaces when indenting
 vim.opt.expandtab=true      -- tabs are spaces
 
 -- UI config
-vim.opt.number=true              -- show line numbers
+vim.opt.number=true
 vim.opt.showcmd=true             -- show command in bottom bar
 vim.opt.showmatch=true           -- highlight matching [{()}] 
 
 -- Leader shortcuts
-vim.g.mapleader=","                  -- leader is comma
-vim.keymap.set("i", "jk", "<ESC>")   -- map jk to escape in insert mode
+vim.g.mapleader=","
+vim.keymap.set("i", "jk", "<ESC>")
 
 -- Searching
 vim.opt.ignorecase=true     -- required for smartcase
 vim.opt.smartcase=true
-vim.keymap.set("n", "<leader><space>", "<cmd>noh<cr>", { desc = "Clear search highlighting" })
+vim.keymap.set(
+    "n",
+    "<leader><space>",
+    "<cmd>noh<cr>",
+    { desc = "Clear search highlighting" }
+)
 
 -- Folding
 vim.opt.foldenable=true                     -- enable folding
@@ -39,11 +47,11 @@ vim.opt.foldmethod="indent"                 -- fold based on indent level
 
 -- Motion
 vim.keymap.set({"n", "v"}, "j", "gj")        -- move by visual line 
-vim.keymap.set({"n", "v"}, "k", "gk")        -- move by visual line 
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")   -- no ^w prefix for split navigation
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")   -- no ^w prefix for split navigation
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>")   -- no ^w prefix for split navigation
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")   -- no ^w prefix for split navigation
+vim.keymap.set({"n", "v"}, "k", "gk")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 
 -- Visual line selection
 vim.api.nvim_create_autocmd("InsertEnter", { command = "set cul" })
