2f691a45e421ebcf969513daabcd354879b658e4
Paul K <pmkoenig@gmail.com>
2026-07-26 13:25:45 -0700

    Add gv alias

--- bash/rc.d/shell.sh
+++ bash/rc.d/shell.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # Basic convenience and shell configuration
+
 alias nv=nvim
+alias bat=batcat
+
+gv() {
+    git ls-files | fzf --query "$1" --bind "enter:become(nvim {})" --preview "batcat --color=always {}"
+}
+
+# fzf shell completion, etc.
+eval "$(fzf --bash)"
+
