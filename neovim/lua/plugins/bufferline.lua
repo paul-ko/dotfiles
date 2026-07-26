0af582e5759b5f0eeae642950cfd81744187d1cb
Paul K <pmkoenig@gmail.com>
2026-07-26 12:16:33 -0700

    Add bufferline nvim plugin

---
+++ neovim/lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'akinsho/bufferline.nvim',
+        version = "4.9.1",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            vim.opt.termguicolors = true
+            require("bufferline").setup{
+                options = {
+                    numbers = { "buffer_id" }
+                }
+            }
+        end
+    }
+}
