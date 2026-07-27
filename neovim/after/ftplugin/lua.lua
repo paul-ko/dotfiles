9f8aa118f1b168c6bd4ceea43ef5f64ceb7eee4c
Paul K <pmkoenig@gmail.com>
2026-07-26 22:46:42 -0700

    Add minor QOL tweaks

--- neovim/after/ftplugin/lua.lua
+++ neovim/after/ftplugin/lua.lua
@@ -3,3 +3,4 @@ vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.expandtab = true
+vim.opt.formatoptions:remove({ "r", "o" })
--- neovim/after/ftplugin/python.lua
+++ neovim/after/ftplugin/python.lua
@@ -1,2 +1,3 @@
 vim.opt_local.foldmethod = "indent"
 vim.opt_local.foldlevel = 99
+vim.opt.formatoptions:remove({ "r", "o" })
