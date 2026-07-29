140228feaac257ef3d0a4b4b1d1161ca258959e7
Paul K <pmkoenig@gmail.com>
2026-07-28 20:34:12 -0700

    Fix cursorline not being constant

--- neovim/lua/config/autocmds.lua
+++ neovim/lua/config/autocmds.lua
@@ -1,7 +1,3 @@
--- Visual line selection
-vim.api.nvim_create_autocmd("InsertEnter", { command = "set cul" })
-vim.api.nvim_create_autocmd("InsertLeave", { command = "set nocul" })
-
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp_attach_add_keymaps", { clear = true }),
   callback = function(args)
