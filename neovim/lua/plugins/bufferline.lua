c373ce877171b23e43038c2e71bde42391ce45bd
Paul K <pmkoenig@gmail.com>
2026-07-30 09:06:41 -0700

    Configure bufferline

--- neovim/lua/plugins/bufferline.lua
+++ neovim/lua/plugins/bufferline.lua
@@ -1,3 +1,5 @@
+-- https://github.com/akinsho/bufferline.nvim
+-- :h bufferline-configuration
 return {
   {
     "akinsho/bufferline.nvim",
@@ -7,8 +9,18 @@ return {
       require("bufferline").setup({
         options = {
           numbers = { "buffer_id" },
+          diagnostics = "nvim_lsp", -- Causes the buffer's name to change color if it has errors or warns
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File explorer",
+              highlight = "Directory",
+              separator = true,
+            },
+          },
         },
       })
+      vim.keymap.set("n", "gb", "<cmd>BufferLinePick<cr>", { desc = "Trigger bufferline tab picker" })
     end,
   },
 }
