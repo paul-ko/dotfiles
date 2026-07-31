9804055f23418d9eb2446be2ddb16ad3443e385a
Paul K <pmkoenig@gmail.com>
2026-07-30 20:51:19 -0700

    Add vsb custom command

---
+++ neovim/lua/config/commands.lua
@@ -0,0 +1,5 @@
+-- Example below
+-- Commands must start with an upper-case letter
+
+-- vim.api.nvim_create_user_command("Vsb", "vert sb <args>", { nargs = "*" })
+vim.cmd("cnoreabbrev vsb vert sb")
