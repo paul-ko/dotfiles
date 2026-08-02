3a14fd267617ac0beeaccc12e0dcc57451a8f7fd
Paul K <pmkoenig@gmail.com>
2026-08-01 17:27:55 -0700

    Remove custom command replaced by plugin

--- neovim/lua/config/commands.lua
+++ neovim/lua/config/commands.lua
@@ -12,21 +12,3 @@ vim.api.nvim_create_user_command("Me", function(opts) f(opts.fargs[1]) end, { na
 -- Abbreviations
 vim.cmd("cnoreabbrev vsb vert sb")
 vim.cmd("cnoreabbrev halp help helphelp")
-
-vim.api.nvim_create_user_command("Close", function()
-  local buf_to_delete = vim.api.nvim_get_current_buf()
-  local win = vim.api.nvim_get_current_win()
-
-  local alt = vim.fn.bufnr("#")
-  if alt ~= buf_to_delete and alt ~= -1 and vim.fn.buflisted(alt) == 1 then
-    vim.api.nvim_win_set_buf(win, alt)
-  else
-    vim.cmd("bprevious")
-  end
-
-  if vim.api.nvim_get_current_buf() == buf_to_delete then
-    vim.cmd("enew") -- nothing else to switch to; don't delete out from under ourselves
-  end
-
-  vim.api.nvim_buf_delete(buf_to_delete, {})
-end, {})
