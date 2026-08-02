e34bc06aa707ad2e63a66b6b60e99472e7217822
Paul K <pmkoenig@gmail.com>
2026-08-01 23:27:29 -0700

    Add ^w help via which-key

--- neovim/lua/plugins/which-key.lua
+++ neovim/lua/plugins/which-key.lua
@@ -6,9 +6,13 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      plugins = {
+        presets = {
+          -- shows the built-in <C-w> window commands (split, close, nav, ...)
+          -- when the prefix is pressed and held briefly
+          windows = true,
+        },
+      },
     },
     keys = {
       {
